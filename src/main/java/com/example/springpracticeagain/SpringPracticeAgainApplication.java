99d49a1b9bac3f68940130c52acb63f367f10bbf
Abi Kunkle <458514@genpt.net>
2023-01-18 23:42:11 -0600

    Initial commit

---
+++ src/main/java/com/example/springpracticeagain/SpringPracticeAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpracticeagain/SpringPracticeAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
