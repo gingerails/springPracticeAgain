5a28c48ecb08277c5d77ffd875fc7f7bb60b81c3
Abi Kunkle <458514@genpt.net>
2023-02-01 11:46:33 -0600

    is this "best practice"? tried to add some abstraction using the CloudStorageService.

--- src/main/java/com/example/springpracticeagain/Controller/GcsController.java
+++ src/main/java/com/example/springpracticeagain/Controller/GcsController.java
@@ -2,10 +2,7 @@ package com.example.springpracticeagain.Controller;
 
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.WritableByteChannel;
 import java.nio.charset.Charset;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import com.example.springpracticeagain.Service.CloudStorageService;
 import com.google.api.gax.paging.Page;
@@ -17,7 +14,6 @@ import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.google.common.base.Charsets.UTF_8;
 @Slf4j
 @RestController
 public class GcsController {
@@ -26,9 +22,6 @@ public class GcsController {
     @Value("gs://abi-buckets/Incoming/blob1")
     private Resource gcsFile;
 
-    @Value("gs://abi-buckets/Processing/")
-    private Resource processingFile;
-
     @GetMapping("/read")
     public String readGcsFile() throws IOException {
         return StreamUtils.copyToString(
@@ -36,78 +29,26 @@ public class GcsController {
                 Charset.defaultCharset());
     }
 
-    @GetMapping("/moveToProcessing")
-    public void readIncomingFolder() throws IOException {
-        Storage storage = StorageOptions.getDefaultInstance().getService();
-        AtomicInteger itemCount = new AtomicInteger(); // Lazy counting the items in the folder
-        Page<Blob> incomingBlobs =
-                storage.list(
-                        "abi-buckets",
-                        Storage.BlobListOption.prefix("Incoming"));
-        incomingBlobs.iterateAll().forEach(blob -> {
-            itemCount.set(itemCount.get() + 1);     // cant just do itemcount++?? bc lambda.  have to use a wrapper
-            //log.info(blob.getName());
-        });
-        log.info("ITEM COUNT: " + itemCount);
+    @GetMapping("/countIncoming")
+    public void readIncomingFolder() {
+        Page<Blob> incomingFolderBlobs = cloudStorageService.getFolder("Incoming");
+        cloudStorageService.countItemsInFolder(incomingFolderBlobs); // log the number of items in the incoming folder
 
-    }
-
-    @GetMapping("/createFiles") // Print Folder Contents
-    public void createFiles(){
-        Storage storage = StorageOptions.getDefaultInstance().getService();
-        Bucket bucket = storage.get("abi-buckets");
-        // make a bunch of objects?
-        for (int i = 0; i < 40; i++) {
-            BlobId blobId = saveString("Incoming/blob" + i, "abcdegdjejsnyebdek", bucket);
-            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
-            Blob blob = storage.create(blobInfo,"abcdegdjejsnyebdek".getBytes(UTF_8));
-        }
-    }
-
-    @GetMapping("/printFolderContents") // Print Folder Contents
-    public void printIncomingFolderContents() throws IOException {
-        Storage storage = StorageOptions.getDefaultInstance().getService();
-        Page<Blob> incomingPage = folder("Incoming");
-     //   Page<Blob> processingPage = folder("Processing");
-
-        incomingPage.iterateAll().forEach(blob -> {
-            log.info(blob.getName());
-        });
     }
 
     /**
-     * Return folder contents (given directory as string). Might not need its own method, we will see.
-     *
-     * @param directoryPrefix
-     * @return
+     * for testing purposes only
      */
-    private Page folder(String directoryPrefix) {
-        Storage storage = StorageOptions.getDefaultInstance().getService();
-        Page<Blob> blobs =
-                storage.list(
-                        "abi-buckets",
-                        Storage.BlobListOption.prefix(directoryPrefix));
-        return blobs;
+    @GetMapping("/createFiles") // Print Folder Contents
+    public void createFiles(){
+        cloudStorageService.createDummyFiles();
     }
 
-
-    // For Testing purposes:
-
-
-    // Save a string to a blob
-    private BlobId saveString(String blobName, String value, Bucket bucket) {
-        byte[] bytes = value.getBytes(UTF_8);
-        Blob blob = bucket.create(blobName, bytes);
-        return blob.getBlobId();
+    @GetMapping("/printFolderContents") // Print Folder Contents
+    public void printIncomingFolderContents() {
+        Page<Blob> incomingPage = cloudStorageService.getFolder("Incoming");
+        incomingPage.iterateAll().forEach(blob -> log.info(blob.getName()));
     }
 
-    public void addFiles(BlobId blobId, String newString, Storage storage) throws IOException {
-        Blob blob = storage.get(blobId);
-        if (blob != null) {
-            WritableByteChannel channel = blob.writer();
-            channel.write(ByteBuffer.wrap(newString.getBytes(UTF_8)));
-            channel.close();
-        }
-    }
 
 }
--- src/main/java/com/example/springpracticeagain/Service/CloudStorageService.java
+++ src/main/java/com/example/springpracticeagain/Service/CloudStorageService.java
@@ -1,38 +1,74 @@
 package com.example.springpracticeagain.Service;
 
-import com.example.springpracticeagain.Controller.GcsController;
 import com.google.api.gax.paging.Page;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.cloud.storage.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.google.common.base.Charsets.UTF_8;
 
+@Slf4j
 @Service
 public class CloudStorageService {
 
-    @Autowired
-    GcsController gcsController;
-
-    public void getIncomingFolderContents() throws IOException {
+    /**
+     * Return folder contents as Page of blobs. (given directory as string).
+     */
+    public Page getFolder(String directoryPrefix) {
         Storage storage = StorageOptions.getDefaultInstance().getService();
-        BlobId blobId = BlobId.of("abi-buckets", "Incoming");
+        Page<Blob> blobs =
+                storage.list(
+                        "abi-buckets",
+                        Storage.BlobListOption.prefix(directoryPrefix));
+        return blobs;
+    }
+
+    /**
+     * Given a Page of blobs, iterate through and log the count.
+     * Next steps are to compare num items to the static config numbers
+     */
+    public void countItemsInFolder(Page<Blob> folder) {
+        AtomicInteger itemCount = new AtomicInteger(); // Lazy counting the items in the folder
+        folder.iterateAll().forEach(blob -> {
+            itemCount.set(itemCount.get() + 1);     // cant just do itemcount++?? bc lambda.  have to use a wrapper
+        });
+        log.info("ITEM COUNT: " + itemCount);
+    }
+
+    /**
+     * for testing purposes only.
+     * adds 40 items to the incoming folder
+     */
+    public void createDummyFiles(){
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+        Bucket bucket = storage.get("abi-buckets");
+        // make a bunch of objects?
+        for (int i = 0; i < 40; i++) {
+            BlobId blobId = saveString("Incoming/blob" + i, "abcdegdjejsnyebdek", bucket);
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
+            storage.create(blobInfo,"abcdegdjejsnyebdek".getBytes(UTF_8));
+        }
+    }
+
+    // Save a string to a blob - for creating dummy text files
+    public BlobId saveString(String blobName, String value, Bucket bucket) {
+        byte[] bytes = value.getBytes(UTF_8);
+        Blob blob = bucket.create(blobName, bytes);
+        return blob.getBlobId();
+    }
+
+    public void updateBlob(BlobId blobId, String newString, Storage storage) throws IOException {
         Blob blob = storage.get(blobId);
         if (blob != null) {
-            byte[] prevContent = blob.getContent();
-            System.out.println(new String(prevContent, UTF_8));
             WritableByteChannel channel = blob.writer();
-            channel.write(ByteBuffer.wrap("Updated content".getBytes(UTF_8)));
+            channel.write(ByteBuffer.wrap(newString.getBytes(UTF_8)));
             channel.close();
         }
-
     }
 
 
